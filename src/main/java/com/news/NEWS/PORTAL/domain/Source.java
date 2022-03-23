5a4e1e6013bdf4b35f5450c504c8892b38918f04
Mariusz <mariusz_podsiadlly@o2.pl>
2022-03-23 18:58:06 +0100

    ADD JSON CLASSES MODELS

---
+++ src/main/java/com/news/NEWS/PORTAL/domain/Source.java
@@ -0,0 +1,54 @@
+
+package com.news.NEWS.PORTAL.domain;
+
+import javax.annotation.Generated;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+@Generated("jsonschema2pojo")
+public class Source {
+
+    @SerializedName("id")
+    @Expose
+    private Object id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+
+    public Object getId() {
+        return id;
+    }
+
+    public void setId(Object id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 1;
+        result = ((result* 31)+((this.name == null)? 0 :this.name.hashCode()));
+        result = ((result* 31)+((this.id == null)? 0 :this.id.hashCode()));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if ((other instanceof Source) == false) {
+            return false;
+        }
+        Source rhs = ((Source) other);
+        return (((this.name == rhs.name)||((this.name!= null)&&this.name.equals(rhs.name)))&&((this.id == rhs.id)||((this.id!= null)&&this.id.equals(rhs.id))));
+    }
+
+}
