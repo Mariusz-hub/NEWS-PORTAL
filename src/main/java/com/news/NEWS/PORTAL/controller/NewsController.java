56def2a1dd5156ae705e1c0b0da14822b28b046e
Mariusz <mariusz_podsiadlly@o2.pl>
2022-03-28 21:43:49 +0200

    change Article,NewsController,News Service

--- src/main/java/com/news/NEWS/PORTAL/controller/NewsController.java
+++ src/main/java/com/news/NEWS/PORTAL/controller/NewsController.java
@@ -1,6 +1,8 @@
 package com.news.NEWS.PORTAL.controller;
 
 import com.news.NEWS.PORTAL.domain.Article;
+import com.news.NEWS.PORTAL.domain.ArticleDto;
+import com.news.NEWS.PORTAL.domain.News;
 import com.news.NEWS.PORTAL.service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.stream.Collectors;
 
+
 @RestController
 @RequestMapping("/api")
 public class NewsController {
 
     private final NewsService newsService;
+
 
     @Autowired
     public NewsController(NewsService newsService) {
@@ -23,10 +27,9 @@ public class NewsController {
     }
 
     @GetMapping("/articles/{category}/{country}")
-    public List<Article> getArticles(@PathVariable String category, @PathVariable String country){
-       List<Article> articles = newsService.getArticles(category,country);
-       List<Article> authors = articles.stream().map(article -> new Article(article.getAuthor(),article.getTitle(),article.getDescription()))
-               .collect(Collectors.toList());
-       return authors;
+    public List<ArticleDto> getArticles(@PathVariable String category, @PathVariable String country){
+        List<ArticleDto> articleDtos = newsService.getArticles(category,country);
+       return articleDtos;
+
     }
 }
--- src/main/java/com/news/NEWS/PORTAL/service/NewsService.java
+++ src/main/java/com/news/NEWS/PORTAL/service/NewsService.java
@@ -1,11 +1,13 @@
 package com.news.NEWS.PORTAL.service;
 
 import com.news.NEWS.PORTAL.domain.Article;
+import com.news.NEWS.PORTAL.domain.ArticleDto;
 import com.news.NEWS.PORTAL.domain.News;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class NewsService {
@@ -14,16 +16,20 @@ public class NewsService {
     private static final String API_KEY = "a6a214f2d26044fb9619e7987a2595b6";
     private RestTemplate restTemplate= new RestTemplate();
 
-    public List<Article> getNewsFor(String category,String country) {
+    public News getNewsFor(String category,String country) {
         News news =  callGetMethod("top-headlines?category={category}&country={country}&apiKey={apiKey}",
                 News.class,category,country,API_KEY);
 
-        return news.getArticles();
+        return news;
     }
 
-    public List<Article> getArticles(String category, String country){
-        List<Article> articleList = getNewsFor(category,country);
-        return articleList;
+    public List<ArticleDto> getArticles(String category, String country){
+        News news = getNewsFor(category,country);
+        List<Article> articles = news.getArticles();
+        List<ArticleDto> articleDtos = articles.stream()
+                .map(article -> new ArticleDto(article.getAuthor(), article.getTitle(), article.getDescription(), article.getUrl(), article.getUrlToImage()))
+                .collect(Collectors.toList());
+        return articleDtos;
     }
 
 
